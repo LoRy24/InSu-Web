a5abeb780ab930b870397650b696c085129151c9
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-01-20 13:54:00 +0100

    Modificate le UTILS per i FabLabs e creati i Tags per le attività e categorie

--- src/lib/insu/fablabs/FabLab.ts
+++ src/lib/insu/fablabs/FabLab.ts
@@ -1,11 +1,11 @@
-class FabLab {
+export class FabLab {
     // Details
-    id: string;
+    id: number;
     name: string;
     address: string;
     city: string;
     state: string;
-    zipcode: string;
+    zipcode: number;
     description: string;
     topics_ids: string[];
     active: boolean;
@@ -14,12 +14,12 @@ class FabLab {
     card_banner_url: string;
 
     // Default Constructor
-    constructor(id: string,
+    constructor(id: number,
                 name: string,
                 address: string,
                 city: string,
                 state: string,
-                zipcode: string,
+                zipcode: number,
                 description: string,
                 topics_ids: string[],
                 active: boolean,
@@ -35,4 +35,40 @@ class FabLab {
         this.active = active;
         this.card_banner_url = card_banner_url;
     }
+}
+
+export enum FabLabTag {
+    TECH_INFO,
+    TECH_NETWORKS,
+    TECH_ELECTRONICS,
+    TECH_MOBILE,
+    TECH_WEB,
+    TECH_DESIGN,
+    TECH_MECHANICAL,
+
+    ART_PAINT,
+    ART_SCULPTURE,
+    ART_WRITING,
+    ART_MUSIC,
+
+    TEXTILE_ECOPRINT,
+    TEXTILE_TAILORING,
+    TEXTILE_EMBROIDERY,
+    TEXTILE_CROCHET,
+    TEXTILE_PRINTING,
+
+    AGRICOLTURE_VEGETABLE_GARDENS,
+    AGRICOLTURE_HYDROPONIC_GREENHOUSES,
+    AGRICOLTURE_BEEHIVES,
+    AGRICOLTURE_COMPOSTING,
+    AGRICOLTURE_FLORICOLTURE,
+    AGRICOLTURE_OFFICINAL,
+
+    CRAFTSMANSHIP_WOOD,
+    CRAFTSMANSHIP_METAL,
+    CRAFTSMANSHIP_LEATHER,
+    CRAFTSMANSHIP_GLASS,
+    CRAFTSMANSHIP_MECHANICS,
+    CRAFTSMANSHIP_REPAIR,
+    CRAFTSMANSHIP_SOAPS,
 }
