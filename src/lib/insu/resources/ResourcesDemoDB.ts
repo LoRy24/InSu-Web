29dc6e4246b3343e332cf45c4c774f7d75d8f66c
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-04-20 10:50:51 +0200

    Aggiornate le pagine delle risorse e della school bank con una prima versione
    Signed-off-by: Lorenzo Rocca <lorenzo.rocca.work@gmail.com>

--- src/lib/insu/resources/ResourcesDemoDB.ts
+++ src/lib/insu/resources/ResourcesDemoDB.ts
@@ -1,16 +1,7 @@
 import {Resource, ResourceType} from "@/lib/insu/resources/Resource";
 
 export const ResourcesDB: Resource[] = [
-    new Resource(
-        0,
-        "Prova",
-        "Risorsa di prova",
-        "Descrizione lunga",
-        ResourceType.SCHOOL_BANK,
-        new Date(),
-        "/app_data/images/cat.png",
-        []
-    )
+
 ];
 
 export function collectByType(type: ResourceType): Resource[] {
