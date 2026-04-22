c997ec8efe1aeab49fdc564d449e73256701e386
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-04-22 15:47:58 +0200

    Aggiornate le card in evidenza
    Signed-off-by: Lorenzo Rocca <lorenzo.rocca.work@gmail.com>

--- src/lib/insu/stats/GeneralDataset.ts
+++ src/lib/insu/stats/GeneralDataset.ts
@@ -1,27 +1,33 @@
 export const featuredStats = [
     {
         id: "stat1-fet",
-        title: "Popolazione residente",
-        description: "Scopri l’andamento della popolazione...",
+        title: "Componenti Serra",
+        description: "Questo file contiene la descrizione del sensore wireless per il monitoraggio" +
+            "dell’umidità e della temperatura del suolo, basato su tecnologia LoRaWAN," +
+            "progettato per applicazioni IIoT a basso consumo con lunga autonomia," +
+            "elevata precisione e resistenza a condizioni ambientali esterne.",
         buttonText: "Visualizza statistica",
         accent: "emerald" as const,
-        href: "/statistiche/popolazione",
+        href: "https://drive.google.com/file/d/1QE8z-qBA7AHLiZJ7cFUSrGdlfrt9XQDk/view?usp=drive_link",
     },
     {
         id: "stat2-fet",
-        title: "Distribuzione per età",
-        description: "Analizza le fasce anagrafiche...",
+        title: "Culture",
+        description: "Il file raccoglie le analisi sulle colture adatte alle fasce climatiche E ed F: ogni elemento è stato valutato tramite 36 domande per determinarne l’idoneità. Include una lista completa con costo al grammo e resa per ettaro, mentre il dataset organizza le informazioni per ciascuna coltura in categorie utili all’integrazione nel progetto.",
         buttonText: "Visualizza statistica",
         accent: "blue" as const,
-        href: "/statistiche/eta",
+        href: "https://drive.google.com/file/d/1vsVdMtQItYPxI2J9FtQT6YgcqIcigBSE/view?usp=drive_link",
     },
     {
         id: "stat3-fet",
-        title: "Densità abitativa",
-        description: "Esplora i dati sulla concentrazione...",
+        title: "Centralina Meteorologica",
+        description: "Questo file contiene la descrizione del sistema integrato per il monitoraggio" +
+            "meteorologico in tempo reale, composto da sensori, datalogger, alimentazione" +
+            "autonoma e moduli di comunicazione, con criteri rigorosi di installazione e" +
+            "manutenzione per garantire dati accurati.",
         buttonText: "Visualizza statistica",
         accent: "orange" as const,
-        href: "/statistiche/densita",
+        href: "https://drive.google.com/file/d/17IuhiLtnnpJ3IjwR8oFOjm-DsQ7Zlsuj/view?usp=drive_link",
     },
 ];
 
